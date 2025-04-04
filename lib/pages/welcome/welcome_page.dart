221249024fd90f1c482be0c919559480d780d315
linemangoma <linemangoma@gmail.com>
2025-04-04 17:07:47 +0100

    implementation de la page d'acceuil

--- lib/pages/welcome/welcome_page.dart
+++ lib/pages/welcome/welcome_page.dart
@@ -5,6 +5,35 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(18),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              child: Image.asset('assets/images/welcome.png'),
+              width: 300,
+              height: 300,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Le paiement le plus rapide au monde",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Intégrez plusieurs méthodes de paiement pour accélérer le processus.",
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            SizedBox(height:50,width: double.infinity,child: ElevatedButton(onPressed: () {}, child: Text("Next"))),
+        
+          ],
+        ),
+      ),
+    );
   }
 }
